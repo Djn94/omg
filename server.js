a3d41c4dbae768b4b7cb04cedbcdcaeaed257c21
Non Dewman <Dnewman1934@yahoo.com>
2019-08-13 15:50:08 -0500

    pls

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/ANGULAR-DEPLOY'));
+app.use(express.static(__dirname + '/dist/angular-deploy'));
 
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/ANGULAR-DEPLOY/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/angular-deploy/index.html'));
 });
 
 app.listen(process.env.PORT || 4200);
